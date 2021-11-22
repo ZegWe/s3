d0600b8d04eaa2d0de23cd447679c9a12131ddc9
ZegWe <abdccdba147@gmail.com>
2021-11-23 00:44:49 +0800

    room table

--- code/Lua/resource.lua
+++ code/Lua/resource.lua
@@ -190,4 +190,14 @@ Resource.RoomDoorInside = {
     Door = "",
 }
 
+Resource.RoomTable = {
+    Table = "",
+    TableInfo = "",
+    NewsPaper1 = "",
+    NewsPaper2 = "",
+    NewsPaper3 = "",
+    Phone = "",
+    DrawPaper = "",
+}
+
 return Resource
